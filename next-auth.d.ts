8248e18f4377e33976575ef5dba723c1d8088bf6
Watthachai <wattchaichai@gmail.com>
2024-12-27 17:35:34 +0700

    Type error: Property 'name' does not exist on type

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -7,6 +7,7 @@ declare module "next-auth" {
     interface Session {
         user: {
             id: string,
+            name: string,
             role: string,
             email: string,
         } & DefaultSession
